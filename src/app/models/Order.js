a8797943d3219cbc223ff784a2c754d1b7eb916e
carlosjunod <carlos@junod.cl>
2017-09-03 15:35:11 -0400

    adding auto price calculation inside Order model

--- src/app/models/Order.js
+++ src/app/models/Order.js
@@ -1,7 +1,30 @@
 var mongoose = require('mongoose')
+const Product = mongoose.model('Product')
+
 var orderSchema = mongoose.Schema({
-  product_id: {type:  mongoose.Schema.Types.ObjectId, ref: 'Product'},
-  total_price: {type: Number}
+  product_id: [{type:  mongoose.Schema.Types.ObjectId, ref: 'Product'}],
+  total_price: getTotal(this.product_id)
 })
 
 module.exports = mongoose.model('Order', orderSchema)
+
+
+function getTotal(arrId){
+  
+  if (arrId.length === 0) {
+    return 0;
+  }
+  
+  let totalPrice = 0
+
+  arrId.forEach((id) => {
+   let prod = new Product()
+   prod.findById(id, (product)=>{
+    totalPrice += product.price
+   })
+  });
+
+  
+  
+  return totalPrice;
+}
