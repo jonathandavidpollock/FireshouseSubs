33957c45421146b5a315679b2e1f061f51542712
Jonathan Pollock <jonathanpollock@Jonathans-MacBook-Pro.local>
2017-09-05 10:06:38 -0400

    static routes in place

--- src/app/controllers/orders.js
+++ src/app/controllers/orders.js
@@ -14,22 +14,83 @@ module.exports = function(app){
   // Get Orders
   // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   router.get('/order', (req, res) => {
-    order.all((err) => {
-      res.status(500).json(err); // errorCallback
-    }, (data) => {
-      res.status(200).json(data); // successCallback
-    });
+    // order.all((err) => {
+    //   res.status(500).json(err); // errorCallback
+    // }, (data) => {
+    //   res.status(200).json(data); // successCallback
+    // });
+    res.json(
+      {
+        'orders': [{
+          "_id": "59adc175278c0a6335c483f5",
+          "__v": 0,
+          "total_price": 13.98,
+          "product_id": [
+              {
+                  "_id": "59ac17689421581a6a87dc9a",
+                  "name": "Smokehouse beef & cheddar brisket",
+                  "price": 6.99,
+                  "__v": 0
+              },
+              {
+                  "_id": "59ac17689421581a6a87dc9a",
+                  "name": "Smokehouse beef & cheddar brisket",
+                  "price": 6.99,
+                  "__v": 0
+              }
+          ]
+      },  {
+        "_id": "59adc175278c0a6335c483f5",
+        "__v": 0,
+        "total_price": 13.98,
+        "product_id": [
+            {
+                "_id": "59ac17689421581a6a87dc9a",
+                "name": "Smokehouse beef & cheddar brisket",
+                "price": 6.99,
+                "__v": 0
+            },
+            {
+                "_id": "59ac17689421581a6a87dc9a",
+                "name": "Smokehouse beef & cheddar brisket",
+                "price": 6.99,
+                "__v": 0
+            }
+        ]
+    }
+        ]})
   });
 
   // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   //
   // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   router.get('/order/:orderID', (req, res) => {
-    const reqBody = req.body;
-    order.one(req.body, (err) => {
-      res.status(500).json(err); // errorCallback
-    }, (data) => {
-      res.status(200).json(data); // successCallback
+    // const reqBody = req.body;
+    // order.one(req.body, (err) => {
+    //   res.status(500).json(err); // errorCallback
+    // }, (data) => {
+    //   res.status(200).json(data); // successCallback
+    // });
+    res.json({
+      order: {
+        "_id": "59adc175278c0a6335c483f5",
+        "__v": 0,
+        "total_price": 13.98,
+        "product_id": [
+            {
+                "_id": "59ac17689421581a6a87dc9a",
+                "name": "Smokehouse beef & cheddar brisket",
+                "price": 6.99,
+                "__v": 0
+            },
+            {
+                "_id": "59ac17689421581a6a87dc9a",
+                "name": "Smokehouse beef & cheddar brisket",
+                "price": 6.99,
+                "__v": 0
+            }
+        ]
+    }
     });
   })
 
