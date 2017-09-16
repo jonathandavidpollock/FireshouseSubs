4d1eb7b44d08084c7a7958687b694fe51fd5d73f
Mykah <mykah.taylor@gmail.com>
2017-09-16 13:26:56 -0400

    Order controller routes and functionality Upadate and Add total price are working

--- src/app/controllers/orders.js
+++ src/app/controllers/orders.js
@@ -47,30 +47,47 @@ module.exports = function(app){
     // console.log('Create NEW Order:', req.body);
     const newOrder = new Order(req.body)
     let total = []
+    let i = 0
     req.body.product_id.forEach((id) => {
       Product.findById(id, (err, product) => {
-        // console.log(product.price);
-        total.push(product.price);
-        // console.log("Before TOTAL-----> ", total)
+        i++
+        total.push(product.price)
+        if(i === req.body.product_id.length){
+          newOrder.total_price = newOrder.getTotalPrice(total)
+          newOrder.save((err, order) => {
+          if(err) return res.send(err);
+            res.json(order);
+          })
+        }      
       })
-    })
-    newOrder.save((err, order) => {
-      if(err) return res.send(err);
-      console.log(total)
-      order.total_price = newOrder.getTotalPrice(total)
-      console.log(order);
-      res.json(order);
     })
   })
 
   // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  // Getting a order by Id and updating it.
+  // Getting an order by Id and updating it.
   // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
   router.put('/order/:orderID', (req, res) => {
     console.log("Update Order now: ", req.body )
-    Order.findOneAndUpdate({_id: req.params.orderID}, req.body,{ new: true }, (err, order) => {
-      res.json(order);
-    })
+    let arryTotal = []
+    let i = 0
+    req.body.product_id.forEach((id) => {
+      Product.findById(id, (err, product) => {
+        i++
+        arryTotal.push(product.price)
+        if(i === req.body.product_id.length){
+         Order.findOne({_id: req.params.orderID}, (err, order) => {
+            order.total_price = order.getTotalPrice(arryTotal)
+            order.product_id = req.body.product_id
+            console.log("3: ", order)
+            order.save((err, order) => {
+              if(err) return res.send(err);
+              console.log("hello")
+              res.json(order);
+            });
+          })
+        }
+      })
+    })    
   })
 
   // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -85,3 +102,4 @@ module.exports = function(app){
 
   return router;
 }
+
